178763f28ec3dd3479899d0cad5bfa178f0bb940
MandarHirap <75581018+MandarHirap@users.noreply.github.com>
2025-11-23 01:32:23 +0530

    admin skeleton

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function (req, res) {
+  res.json("created user successfully");
+});
+
+adminRouter.post("/signin", function (req, res) {
+  res.json("you are signed in");
+});
+
+adminRouter.post("/course", function (req, res) {
+  res.json("you are signed in");
+});
+
+adminRouter.put("/course", function (req, res) {
+  res.json("you are signed in");
+});
+
+adminRouter.get("/course/bulk", function (req, res) {
+  res.json("you are signed in");
+});
+
+module.exports = {
+  adminRouter: adminRouter,
+};
