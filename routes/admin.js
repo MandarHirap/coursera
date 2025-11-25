cb3499e37c9e7f54c5413185a8b2181cdc241f0f
MandarHirap <75581018+MandarHirap@users.noreply.github.com>
2025-11-25 08:01:42 +0530

    admin side signup/signin logic with token

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,77 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
-adminRouter.post("/signup", function (req, res) {
-  res.json("created user successfully");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const jwt_secret = "coursera";
+const salt_rounds = 10;
+
+adminRouter.post("/signup", async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password is required" });
+    }
+    const exisitingAdmin = await adminModel.findOne({ email });
+    if (exisitingAdmin) {
+      return res.status(400).json({ message: "Admin already exists" });
+    }
+    const hashedpassword = await bcrypt.hash(password, salt_rounds);
+
+    await adminModel.create({
+      email,
+      password: hashedpassword,
+    });
+    // return res.json({"admin created successfully"});
+    return res.json({ message: "Admin created successfully" });
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-adminRouter.post("/signin", function (req, res) {
+adminRouter.post("/signin", async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    const admin = await adminModel.findOne({ email });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    if (!admin) {
+      return res.status(400).json({ message: "Admin not found" });
+    }
+    const ispasswordvalid = await bcrypt.compare(password, admin.password);
+    if (!ispasswordvalid) {
+      return res.status(401).json({ message: "Incorrect password " });
+    }
+    const token = jwt.sign(
+      {
+        id: admin._id,
+        email: admin.email,
+      },
+      jwt_secret,
+      { expiresIn: "1h" }
+    );
+    return res.json({ message: "signed in", token });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+adminRouter.post("/course", function (req, res) {
   res.json("you are signed in");
 });
 
-adminRouter.post("/", function (req, res) {
+adminRouter.put("/course", function (req, res) {
   res.json("you are signed in");
 });
 
-adminRouter.put("/", function (req, res) {
-  res.json("you are signed in");
-});
-
-adminRouter.get("/bulk", function (req, res) {
+adminRouter.get("/course/bulk", function (req, res) {
   res.json("you are signed in");
 });
 
