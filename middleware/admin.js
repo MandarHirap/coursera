28ed4fd213d31ea6de68ef855e29d43823d9d8e9
MandarHirap <75581018+MandarHirap@users.noreply.github.com>
2025-11-27 00:27:53 +0530

    middleware for admin

---
+++ middleware/admin.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { admin_jwt_secret_jwt_secret } = require("../config");
+
+function adminmiddleware(req, res, next) {
+  const token = req.header.token;
+  const decoded = jwt.verify(token, user_jwt_secret);
+
+  if (decoded) {
+    req.adminId = decodedId;
+    next();
+  } else {
+    res.status(403).json({
+      message: "you are not signed in as an admin",
+    });
+  }
+}
+
+module.exports = {
+  adminmiddleware: adminmiddleware,
+};
